f9a5efb153d88503b491050537a9fde6834fa87f
Calina Cenan <calina@cenan.net>
2023-05-16 08:01:04 +0000

    Add root health check.

--- app/routers/health_check.py
+++ app/routers/health_check.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/health")
 def health_check():
+    return {"status": "ok"}
+
+
+@router.get("/")
+def root_health_check():
     return {"status": "ok"}
